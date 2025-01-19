13f1f5117fca71bbb73559a35012dac40b445765
ShiftR <119271753+Blekuo@users.noreply.github.com>
2025-01-19 22:47:12 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -274,12 +274,89 @@ int mid, int day)
         currentMonth(that12, day);
     }
 }
+void usrIn(char *userInput, short size)
+{
+    char *dest = userInput;
+    scanf("%10s", dest);
+    dest = NULL;
+}
+void CalenderSystem9(struct month m_1,struct month m_2,struct month m_3,struct month m_4
+,struct month m_5,struct month m_6,struct month m_7,struct month m_8,
+struct month m_9,struct month m_10,struct month m_11,struct month m_12)
+{
+    textWrapper("CalenderSystem");
+
+    char userInput[30];
+    char **commandList = (char**)malloc(100*sizeof(double));
+
+    int mid;
+    int day;
+
+    commandList[0] = "setCurrent";
+    commandList[1] = "showCommand";
+    commandList[2] = "addEvent";
+
+    char **cLptr = commandList;
+
+    printf("showCommand for guide\n");
+    printf("\e[0;34m<-=\e[1;33m(o)\e[0;34m=->\e[0m ");
+    usrIn(userInput, sizeof(userInput));
+
+    if (!strcmp(userInput, commandList[1])) 
+    {
+        while (*cLptr)
+        {
+            printf("[%s] \n", *cLptr);
+            cLptr++;
+        }
+        cLptr = commandList;
+    }
+    if (!strcmp(userInput, commandList[0])) 
+    {
+        while (1) 
+        {
+            printf("Set Current Month(id): ");
+            scanf("%2d", &mid);
+
+            printf("Set Current Day: ");
+            scanf("%2d", &day);
+            if (mid > 12 || day > 31) 
+            {
+                clearit();
+                printf("please input the right format (id/day)\n");
+            }
+            else
+            {
+                break;
+            }
+        }
+        checkMD(m_1, m_2, m_3, m_4, 
+        m_5, m_6, m_7, m_8,
+        m_9, m_10, m_11, m_12, 
+        mid, day);
+    }
+
+    textWrapper("");
+
+    newline();
+    mid = 0;
+    day = 0;
+    printf("Press Any Key To Continue...");
+    system("sleep 3");
+    fgets(userInput, sizeof(userInput), stdin);
+    strcpy(userInput, "");
+
+    newline();
+
+    // main(argc, argv);
+    clearit();
+    CalenderSystem9(m_1, m_2, m_3, m_4, m_5, m_6, m_7, m_8, m_9, m_10, m_11, m_12);
+}
 int main(int argc, char **argv)
 {
     clearit();
     short id = 1;
-    textWrapper("CalenderSystem");
-    
+
     struct year y_1;
     y_1.num = 2025;
     
@@ -333,18 +410,18 @@ int main(int argc, char **argv)
     m_11.name = "November";
     m_12.name = "December";
 
-    m_1.dateList = (int*)malloc(50*sizeof(int));
-    m_2.dateList = (int*)malloc(50*sizeof(int));
-    m_3.dateList = (int*)malloc(50*sizeof(int));
-    m_4.dateList = (int*)malloc(50*sizeof(int));
-    m_5.dateList = (int*)malloc(50*sizeof(int));
-    m_6.dateList = (int*)malloc(50*sizeof(int));
-    m_7.dateList = (int*)malloc(50*sizeof(int));
-    m_8.dateList = (int*)malloc(50*sizeof(int));
-    m_9.dateList = (int*)malloc(50*sizeof(int));
-    m_10.dateList = (int*)malloc(50*sizeof(int));
-    m_11.dateList = (int*)malloc(50*sizeof(int));
-    m_12.dateList = (int*)malloc(50*sizeof(int));
+    m_1.dateList = (int*)calloc(31, sizeof(int));
+    m_2.dateList = (int*)calloc(31, sizeof(int));
+    m_3.dateList = (int*)calloc(31, sizeof(int));
+    m_4.dateList = (int*)calloc(31, sizeof(int));
+    m_5.dateList = (int*)calloc(31, sizeof(int));
+    m_6.dateList = (int*)calloc(31, sizeof(int));
+    m_7.dateList = (int*)calloc(31, sizeof(int));
+    m_8.dateList = (int*)calloc(31, sizeof(int));
+    m_9.dateList = (int*)calloc(31, sizeof(int));
+    m_10.dateList = (int*)calloc(31, sizeof(int));
+    m_11.dateList = (int*)calloc(31, sizeof(int));
+    m_12.dateList = (int*)calloc(31, sizeof(int));
 
     m_1.eventName = (char**)malloc(200*sizeof(double));
     m_2.eventName = (char**)malloc(200*sizeof(double));
@@ -405,41 +482,13 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    printf("4.8KB of memory reserved!");
+    printf("%d Bytes of memory reserved!",(((31*4)*12)+(1600*12)));
     newline();
 
     //Initialize
 
     printf("Year %d\n", y_1.num);
 
-    int mid;
-    int day;
-
-    while (1) 
-    {
-        printf("Set Current Month(id): ");
-        scanf("%2d", &mid);
-
-        printf("Set Current Day: ");
-        scanf("%2d", &day);
-        if (mid > 12 || day > 31) 
-        {
-            clearit();
-            printf("please input the right format (id/day)\n");
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    checkMD(m_1, m_2, m_3, m_4, 
-    m_5, m_6, m_7, m_8,
-     m_9, m_10, m_11, m_12, 
-     mid, day);
-
-    textWrapper("");
-
-    newline();
+    CalenderSystem9(m_1, m_2, m_3, m_4, m_5, m_6, m_7, m_8, m_9, m_10, m_11, m_12);
     return (0);
 }
